0518e819bc05fec2bca1fd8d1052ce029be63ade
William <william.backman@gmail.com>
2021-01-15 02:55:36 +0100

    update to use new gql api endpoints, since old one no longer exists

--- index.js
+++ index.js
@@ -1,63 +1,118 @@
-const https = require('https');
+const axios = require('axios');
 
 function get(url) {
     return new Promise((resolve, reject) => {
-        const nUrl = new URL(url);
-        const options = {
-            host: nUrl.host,
-            hostname: nUrl.hostname,
-            origin: nUrl.origin,
-            path: nUrl.pathname + nUrl.search,
-            href: nUrl.href,
+        axios.get(url, {
             headers: {
                 'client-id': clientId,
             },
-        };
-        /* console.log('clientId', clientId);
-        console.log('options', options); */
-        https.get(options, (response) => {
-            // console.log('response', response);
-                let data = {};
-                data.statusCode = response.statusCode;
-                data.body = [];
-                response.on('data', (chunk) => data.body.push(chunk));
-                response.on('end', () => {
-                    data.body = data.body.join('');
-                    resolve(data);
-                });
-            })
-            .on('error', (error) => reject(error));
+        })
+        .then((response) => resolve(response))
+        .catch((error) => reject(error));
     });
 }
 
-function getAccessToken(id, vod) {
+function post(url, data) {
     return new Promise((resolve, reject) => {
-        get(`https://api.twitch.tv/api/${vod ? 'vods' : 'channels'}/${id}/access_token?oauth_token=undefined&need_https=true&platform=web&player_type=picture-by-picture&player_backend=mediaplayer`)
-            .then((data) => {
-                if (data.statusCode != 200) {
-                    // console.log('accessToken', data.body);
-                    reject(new Error(`${JSON.parse(data.body).message}`));
-                } else {
-                    resolve(JSON.parse(data.body));
-                }
-            })
-            .catch((error) => reject(error));
+        axios.post(url, data, {
+            headers: {
+                'client-id': clientId,
+            },
+        })
+        .then((response) => resolve(response))
+        .catch((error) => reject(error));
     });
+}
+
+function getAccessToken(channel, vod, id = "") {
+    return new Promise((resolve, reject) => {
+        const query = makeGraphQlPacket(channel, vod, id);
+        post('https://gql.twitch.tv/gql', JSON.stringify(query)).then((response) => {
+            if (response.status !== 200) {
+                reject(new Error(`${JSON.parse(response.data).message}`));
+            } else {
+                // console.log('data.data', data.data);
+                if (response.data === undefined) {
+                    reject(new Error(`malformed response - missing data: ${data}`));
+                }
+                const data = response.data;
+                // console.log('data', data);
+                if (data.length === 0) {
+                    reject(new Error(`data missing from response: ${data}`));
+                }
+                if (data[0].data === undefined) {
+                    reject(new Error(`data missing from response: ${data}`));
+                }
+                if (!vod && data[0].data.streamPlaybackAccessToken === undefined) {
+                    reject(new Error(`streamPlaybackAccessToken missing from data: ${JSON.stringify(data)}`));
+                } else if (vod && data[0].data.videoPlaybackAccessToken === undefined) {
+                    reject(new Error(`videoPlaybackAccessToken missing from data: ${JSON.stringify(data)}`));
+                }
+                const accessToken = !vod ? data[0].data.streamPlaybackAccessToken : data[0].data.videoPlaybackAccessToken;
+                resolve(accessToken);
+            }
+        }).catch((error) => reject(error));
+    });
+}
+
+function makeGraphQlPacket(channel, vod, id = "") {
+    return [{
+        operationName: 'PlaybackAccessToken_Template',
+        variables: {
+            "isLive": !vod,
+            "isVod": vod,
+            "login": channel,
+            "playerType": "site",
+            "vodID": id,
+        },
+        query: `query PlaybackAccessToken_Template(
+            $login: String!
+            $isLive: Boolean!
+            $vodID: ID!
+            $isVod: Boolean!
+            $playerType: String!
+          ) {
+            streamPlaybackAccessToken(
+              channelName: $login
+              params: {
+                platform: "web"
+                playerBackend: "mediaplayer"
+                playerType: $playerType
+              }
+            ) @include(if: $isLive) {
+              value
+              signature
+              __typename
+            }
+            videoPlaybackAccessToken(
+              id: $vodID
+              params: {
+                platform: "web"
+                playerBackend: "mediaplayer"
+                playerType: $playerType
+              }
+            ) @include(if: $isVod) {
+              value
+              signature
+              __typename
+            }
+          }`,
+    }]
 }
 
 function getPlaylist(id, accessToken, vod) {
     return new Promise((resolve, reject) => {
-        get(`https://usher.ttvnw.net/${vod ? 'vod' : 'api/channel/hls'}/${id}.m3u8?client_id=${clientId}&token=${accessToken.token}&sig=${accessToken.sig}&allow_source&allow_audio_only`)
-            .then((data) => {
-                switch (data.statusCode) {
+        get(`https://usher.ttvnw.net/${vod ? 'vod' : 'api/channel/hls'}/${id}.m3u8?client_id=${clientId}&token=${accessToken.value}&sig=${accessToken.signature}&allow_source&allow_audio_only`)
+            .then((response) => {
+                switch (response.status) {
                     case 200:
-                        resolve(resolve(data.body));
+                        resolve(resolve(response.data));
                         break;
                     case 404:
                         reject(new Error('Transcode does not exist - the stream is probably offline'));
                         break;
                     default:
-                        reject(new Error(`Twitch returned status code ${data.statusCode}`));
+                        reject(new Error(`Twitch returned status code ${response.status}`));
                         break;
                 }
             })
@@ -81,16 +136,16 @@ function parsePlaylist(playlist) {
 
 function getStream(channel, raw) {
     return new Promise((resolve, reject) => {
-        getAccessToken(channel)
+        getAccessToken(channel, false)
             .then((accessToken) => getPlaylist(channel, accessToken))
             .then((playlist) => resolve((raw ? playlist : parsePlaylist(playlist))))
             .catch(error => reject(error));
     });
 }
 
-function getVod(vid, raw) {
+function getVod(channel, vid, raw) {
     return new Promise((resolve, reject) => {
-        getAccessToken(vid, true)
+        getAccessToken(channel, true, vid)
             .then((accessToken) => getPlaylist(vid, accessToken, true))
             .then((playlist) => resolve((raw ? playlist : parsePlaylist(playlist))))
             .catch(error => reject(error));
@@ -107,5 +162,13 @@ module.exports = function (cid) {
 
 /* const twitch = test('kimne78kx3ncx6brgo4mv6wki5h1ko');
 twitch.getStream('mizkif', false).then((result) => {
-    console.log(result);
+    console.log('mizkif stream', result);
+}).catch((error) => {
+    console.log('error', error.message);
+});
+
+twitch.getVod('mizkif', '872482730', false).then((result) => {
+    console.log('mizkif vod', result);
+}).catch((error) => {
+    console.log('error', error.message);
 }); */
